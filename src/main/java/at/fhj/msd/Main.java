a039794dec154b5bb3a223b2913b11b247ddf5c3
Alia Alamer <alia.alamer@edu.fh-joanneum.at>
2025-03-16 00:29:00 +0100

    created Main Class and called the methods in the Calculator class

---
+++ src/main/java/at/fhj/msd/Main.java
@@ -0,0 +1,24 @@
+package at.fhj.msd;
+
+public class Main 
+{
+    public static void main( String[] args ){
+
+        Calculator calculate = new Calculator();
+
+        System.out.println(calculate.add(2, 4));
+
+        System.out.println(calculate.minus(2, 4));
+
+        System.out.println(calculate.multiply(2, 4));
+
+        try{
+        System.out.println(calculate.divide(2, 0));
+        } catch (ArithmeticException a){
+            System.out.println(a.getMessage());
+        }
+
+        System.out.println(calculate.divide(2, 4));
+    }
+    
+}
